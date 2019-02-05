ba38262c86d594d51adc18dc0d97b3e5b75ff962
Trevor Jones <SMSAgentSoftware@users.noreply.github.com>
2019-02-05 11:50:40 +0000

    Initial Commit

---
+++ Delivery Optimization Monitor.ps1
@@ -0,0 +1,151 @@
+﻿##########################################################################
+##                                                                      ##
+##                 DELIVERY OPTIMIZATION MONITOR TOOL                   ##
+##                                                                      ##
+## Description: View Delivery Optimization statistics from the local or ##
+##              remote computer                                         ##
+## Author:      Trevor Jones                                            ##
+## Blog:        smsagent.blog                                           ##
+##                                                                      ##
+##########################################################################
+
+
+# Set the location we are running from
+$Source = $PSScriptRoot
+
+# Load the function library
+. "$Source\bin\FunctionLibrary.ps1"
+
+# Do PS version check
+If ($PSVersionTable.PSVersion.Major -lt 5)
+{
+  $Content = "Delivery Optimization Monitor cannot start because it requires minimum PowerShell 5."
+  New-WPFMessageBox -Content $Content -Title "Oops!" -TitleBackground Orange -TitleTextForeground Yellow -TitleFontSize 20 -TitleFontWeight Bold -BorderThickness 1 -BorderBrush Orange -Sound 'Windows Exclamation'
+  Break
+}
+
+# Do .Net Version Check
+$Release = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full" -Name Release).Release
+If ($Release -lt 394802)
+{
+  $Content = "Delivery Optimization Monitor cannot start because it requires minimum .Net Framework 4.6.2."
+  New-WPFMessageBox -Content $Content -Title "Oops!" -TitleBackground Orange -TitleTextForeground Yellow -TitleFontSize 20 -TitleFontWeight Bold -BorderThickness 1 -BorderBrush Orange -Sound 'Windows Exclamation'
+  Break
+}
+
+# Load the required assemblies
+Add-Type -AssemblyName PresentationFramework,PresentationCore,WindowsBase,System.Drawing
+Add-Type -Path "$Source\bin\System.Windows.Interactivity.dll"
+Add-Type -Path "$Source\bin\ControlzEx.dll"
+Add-Type -Path "$Source\bin\MahApps.Metro.dll"
+Add-Type -Path "$Source\bin\LiveCharts.dll"
+Add-Type -Path "$Source\bin\LiveCharts.Wpf.dll"
+
+# Define the XAML code
+[XML]$Xaml = [System.IO.File]::ReadAllLines("$Source\Xaml\App.xaml") 
+
+# Create a synchronized hash table and add the WPF window and its named elements to it
+$UI = [System.Collections.Hashtable]::Synchronized(@{})
+$UI.Window = [Windows.Markup.XamlReader]::Load((New-Object -TypeName System.Xml.XmlNodeReader -ArgumentList $xaml))
+$xaml.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]") | ForEach-Object -Process {
+    $UI.$($_.Name) = $UI.Window.FindName($_.Name)
+    }
+
+# Set the icon
+$UI.Window.Icon = "$Source\bin\peer.ico"
+
+# Set the local computer initially
+$UI.ComputerName.Text = $env:COMPUTERNAME
+
+# Hold the background jobs here. Useful for querying the streams for any errors.
+$UI.Jobs = @()
+
+# Load in the other code libraries
+. "$Source\bin\ClassLibrary.ps1"
+. "$Source\bin\EventLibrary.ps1"
+
+# OC for data binding source
+$UI.DataSource = New-Object System.Collections.ObjectModel.ObservableCollection[Object]
+$UI.DataSource.Add("False") # ProgressBar Indeterminate
+$UI.DataSource.Add("Ready") # Status
+$UI.DataSource.Add("Black") # Status foreground colour
+$UI.DataSource.Add($null)   # This Month Datagrid itemssource
+$UI.DataSource.Add($null)   # PerfSnap Datagrid itemssource
+$UI.DataSource.Add($null)   # Current Jobs datagrid itemssource
+$UI.DataSource.Add($null)   # Download Chart series collection
+$UI.DataSource.Add($null)   # Upload Chart series collection
+$UI.DataSource.Add("Hidden")# Chart visibility
+
+$UI.Window.DataContext = $UI.DataSource
+
+# Create the chart series
+# Download chart
+$DownloadSeriesCollection = [LiveCharts.SeriesCollection]::new()
+$DownloadSeries = [LiveCharts.Wpf.PieSeries]::new()
+$DownloadSeries.Title = "From Microsoft"
+$DownloadSeriesValue = [LiveCharts.ChartValues[LiveCharts.Defaults.ObservableValue]]::new()
+$DownloadSeriesValue.Add([LiveCharts.Defaults.ObservableValue]::new(0))
+$DownloadSeries.Values = $DownloadSeriesValue
+$DownloadSeries.DataLabels = $true
+$DownloadSeriesCollection.Add($DownloadSeries)
+
+$DownloadSeries = [LiveCharts.Wpf.PieSeries]::new()
+$DownloadSeries.Title = "From PCs on the local network"
+$DownloadSeriesValue = [LiveCharts.ChartValues[LiveCharts.Defaults.ObservableValue]]::new()
+$DownloadSeriesValue.Add([LiveCharts.Defaults.ObservableValue]::new(0))
+$DownloadSeries.Values = $DownloadSeriesValue
+$DownloadSeries.DataLabels = $true
+$DownloadSeriesCollection.Add($DownloadSeries)
+
+$DownloadSeries = [LiveCharts.Wpf.PieSeries]::new()
+$DownloadSeries.Title = "From PCs on the internet"
+$DownloadSeriesValue = [LiveCharts.ChartValues[LiveCharts.Defaults.ObservableValue]]::new()
+$DownloadSeriesValue.Add([LiveCharts.Defaults.ObservableValue]::new(0))
+$DownloadSeries.Values = $DownloadSeriesValue
+$DownloadSeries.DataLabels = $true
+$DownloadSeriesCollection.Add($DownloadSeries)
+
+$UI.DataSource[6] = $DownloadSeriesCollection
+
+# Upload chart
+$UploadSeriesCollection = [LiveCharts.SeriesCollection]::new()
+$UploadSeries = [LiveCharts.Wpf.PieSeries]::new()
+$UploadSeries.Title = "Uploaded to PCs on the internet"
+$UploadSeriesValue = [LiveCharts.ChartValues[LiveCharts.Defaults.ObservableValue]]::new()
+$UploadSeriesValue.Add([LiveCharts.Defaults.ObservableValue]::new(0))
+$UploadSeries.Values = $UploadSeriesValue
+$UploadSeries.DataLabels = $true
+$UploadSeriesCollection.Add($UploadSeries)
+
+$UploadSeries = [LiveCharts.Wpf.PieSeries]::new()
+$UploadSeries.Title = "Uploaded to PCs on the local network"
+$UploadSeriesValue = [LiveCharts.ChartValues[LiveCharts.Defaults.ObservableValue]]::new()
+$UploadSeriesValue.Add([LiveCharts.Defaults.ObservableValue]::new(0))
+$UploadSeries.Values = $UploadSeriesValue
+$UploadSeries.DataLabels = $true
+$UploadSeriesCollection.Add($UploadSeries)
+
+$UI.DataSource[7] = $UploadSeriesCollection
+
+# Region to display the UI
+#region DisplayUI
+
+# If code is running in ISE, use ShowDialog()...
+if ($psISE)
+{
+    $null = $UI.window.Dispatcher.InvokeAsync{$UI.window.ShowDialog()}.Wait()
+}
+# ...otherwise run as an application
+Else
+{
+    # Make PowerShell Disappear
+    $windowcode = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+    $asyncwindow = Add-Type -MemberDefinition $windowcode -Name Win32ShowWindowAsync -Namespace Win32Functions -PassThru
+    $null = $asyncwindow::ShowWindowAsync((Get-Process -PID $pid).MainWindowHandle, 0)
+ 
+    $app = New-Object -TypeName Windows.Application
+    $app.Properties
+    $app.Run($UI.Window)
+}
+
+#endregion
---
+++ bin/EventLibrary.ps1
@@ -0,0 +1,244 @@
+﻿#######################################################################
+##                                                                   ##
+## Defines event handlers used by Delivery Optimization Monitor Tool ##
+##                                                                   ##
+#######################################################################
+
+# Bring the window to the fore
+$UI.Window.Add_Loaded({
+    $This.Activate()
+})
+
+$UI.GO.Add_Click({
+
+    # Reset values
+    $UI.DataSource[0] = $true
+    $UI.DataSource[1] = "Running..."
+    $UI.DataSource[2] = "Black"
+    $UI.DataSource[3] = $null
+    $UI.DataSource[4] = $null
+    $UI.DataSource[5] = $null
+    $UI.DataSource[6][0].Values[0].Value = 0
+    $UI.DataSource[6][1].Values[0].Value = 0
+    $UI.DataSource[6][2].Values[0].Value = 0
+    $UI.DataSource[7][0].Values[0].Value = 0
+    $UI.DataSource[7][1].Values[0].Value = 0
+    $UI.DataSource[8] = "Visible"
+    
+    # Main code to run in background job
+    $Code = {
+        Param($UI,$ComputerName)
+
+        # If local machine
+        If ($ComputerName -eq $env:COMPUTERNAME)
+        {
+            # Test for elevation
+            If (([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+            {
+            }
+            Else
+            {
+                $SoundFile = "$env:SystemDrive\Windows\Media\notify.wav"
+                $SoundPlayer = New-Object System.Media.SoundPlayer -ArgumentList $SoundFile
+                $SoundPlayer.Add_LoadCompleted({
+                    $This.Play()
+                    $This.Dispose()
+                })
+                $SoundPlayer.LoadAsync()
+                $UI.DataSource[1] = "ERROR: Please run as administrator when connecting to the local machine!"
+                $UI.DataSource[2] = "Red"
+                $UI.DataSource[0] = $false
+                Return
+            }
+        }
+        Else
+        {
+            $UI.DataSource[1] = "Testing connectivity to $ComputerName"
+            $TC = Test-Connection -ComputerName $ComputerName -Count 2 -Quiet
+            If ($TC -eq $false)
+            {
+                $SoundFile = "$env:SystemDrive\Windows\Media\notify.wav"
+                $SoundPlayer = New-Object System.Media.SoundPlayer -ArgumentList $SoundFile
+                $SoundPlayer.Add_LoadCompleted({
+                    $This.Play()
+                    $This.Dispose()
+                })
+                $SoundPlayer.LoadAsync()
+                $UI.DataSource[1] = "ERROR: Could not contact $ComputerName on the network!"
+                $UI.DataSource[2] = "Red"
+                $UI.DataSource[0] = $false
+                Return
+            }
+
+            Try
+            {
+                $PSSession = New-PSSession -ComputerName $ComputerName -ErrorAction Stop
+            }
+            Catch
+            {
+                $SoundFile = "$env:SystemDrive\Windows\Media\notify.wav"
+                $SoundPlayer = New-Object System.Media.SoundPlayer -ArgumentList $SoundFile
+                $SoundPlayer.Add_LoadCompleted({
+                    $This.Play()
+                    $This.Dispose()
+                })
+                $SoundPlayer.LoadAsync()
+                $UI.DataSource[1] = "ERROR: Could not create a PS Session to $ComputerName! $_"
+                $UI.DataSource[2] = "Red"
+                $UI.DataSource[0] = $false
+                Return
+            }
+        }
+
+        # Get Delivery Optimization Snapshot This month
+        $UI.DataSource[1] = "Retrieving PerfSnap for this month"
+        If ($ComputerName -eq $env:COMPUTERNAME)
+        {
+            $DOPerfSnapTM = Try{Get-DeliveryOptimizationPerfSnapThisMonth -Verbose -ErrorAction Stop}Catch{}
+        }
+        Else
+        {
+            $DOPerfSnapTM = Invoke-Command -Session $PSSession -ScriptBlock { Try{Get-DeliveryOptimizationPerfSnapThisMonth -Verbose -ErrorAction Stop}Catch{} }
+        }
+        $DOPerfSnapTMTable = New-Object System.Data.DataTable
+
+        If ($DOPerfSnapTM.Count -ge 1 -and $DOPerfSnapTM.GetType().Name -ne "String")
+        {
+            
+            [void]$DOPerfSnapTMTable.Columns.Add("Statistic")
+            [void]$DOPerfSnapTMTable.Columns.Add("Value")
+
+            # Convert to a useable object
+            [pscustomobject]$Obj = @{}
+            Foreach ($Item in $DOPerfSnapTM)
+            {
+                $Obj.Add($Item.Split()[0], $Item.Split()[-1])
+            }
+
+
+            # Percentages
+            $DownloadTotal = ([int]$Obj.MonthlyDownloadHttpBytes + [int]$Obj.MonthlyDownloadLanBytes + [int]$Obj.MonthlyDownloadInternetBytes)
+            $Percent_DownloadHttpBytes = [Math]::Round((100 * ([int]$Obj.MonthlyDownloadHttpBytes / $DownloadTotal)),2)
+            $Percent_MonthlyDownloadLanBytes = [Math]::Round((100 * ([int]$Obj.MonthlyDownloadLanBytes / $DownloadTotal)),2)
+            $Percent_MonthlyDownloadInternetBytes = [Math]::Round((100 * ([int]$Obj.MonthlyDownloadInternetBytes / $DownloadTotal)),2)
+
+            # Populate table
+            [void]$DOPerfSnapTMTable.Rows.Add("Start Date", $Obj.MonthStartDate)
+            [void]$DOPerfSnapTMTable.Rows.Add("Uploaded to PCs on the local network", "$([math]::Round(($Obj.MonthlyUploadLanBytes / 1MB),2)) MB")
+            [void]$DOPerfSnapTMTable.Rows.Add("Uploaded to PCs on the internet", "$([math]::Round(($Obj.MonthlyUploadInternetBytes / 1MB),2)) MB")
+            [void]$DOPerfSnapTMTable.Rows.Add("Downloaded from Microsoft", "$([math]::Round(($Obj.MonthlyDownloadHttpBytes/ 1MB),2)) MB ($Percent_DownloadHttpBytes %)")
+            [void]$DOPerfSnapTMTable.Rows.Add("Downloaded from PCs on your local network", "$([math]::Round(($Obj.MonthlyDownloadLanBytes / 1MB),2)) MB ($Percent_MonthlyDownloadLanBytes %)")
+            [void]$DOPerfSnapTMTable.Rows.Add("Downloaded from PCs on the internet", "$([math]::Round(($Obj.MonthlyDownloadInternetBytes / 1MB),2)) MB ($Percent_MonthlyDownloadInternetBytes %)")
+            [void]$DOPerfSnapTMTable.Rows.Add("Average download speed (user initiated)", "$([math]::Round(($Obj.MonthlyDownloadFgRateKbps / 1024),2)) Mbps")
+            [void]$DOPerfSnapTMTable.Rows.Add("Average download speed (background)", "$([math]::Round(($Obj.MonthlyDownloadBgRateKbps / 1024),2)) Mbps")
+            [void]$DOPerfSnapTMTable.Rows.Add("Monthly Upload Limit Reached", $Obj.MonthlyUploadLimitReached)
+
+            # Populate Charts
+            $UI.DataSource[6][0].Values[0].Value = $([math]::Round(($Obj.MonthlyDownloadHttpBytes/ 1MB),2))
+            $UI.DataSource[6][1].Values[0].Value = $([math]::Round(($Obj.MonthlyDownloadLanBytes / 1MB),2))
+            $UI.DataSource[6][2].Values[0].Value = $([math]::Round(($Obj.MonthlyDownloadInternetBytes / 1MB),2))
+            $UI.DataSource[7][0].Values[0].Value = $([math]::Round(($Obj.MonthlyUploadInternetBytes/ 1MB),2))
+            $UI.DataSource[7][1].Values[0].Value = $([math]::Round(($Obj.MonthlyUploadLanBytes / 1MB),2))
+
+        }
+        ElseIf ($DOPerfSnapTM.GetType().Name -eq "String")
+        {
+            [void]$DOPerfSnapTMTable.Columns.Add("Info")
+            [void]$DOPerfSnapTMTable.Rows.Add("$DOPerfSnapTM")
+        }
+        $UI.DataSource[3] = $DOPerfSnapTMTable
+
+
+
+
+        # Get Get Delivery Optimization Snapshot 
+        $UI.DataSource[1] = "Retrieving PerfSnap long term data"
+        If ($ComputerName -eq $env:COMPUTERNAME)
+        {
+            $DOPerfSnap = Try{Get-DeliveryOptimizationPerfSnap -Verbose -ErrorAction Stop}Catch{}
+        }
+        Else
+        {
+            $DOPerfSnap = Invoke-Command -Session $PSSession -ScriptBlock { Try{Get-DeliveryOptimizationPerfSnap -Verbose -ErrorAction Stop}Catch{} }
+        }       
+        $DOPerfSnapTable = New-Object System.Data.Datatable
+        
+        If ($DOPerfSnap.Count -ge 1 -and $DOPerfSnap.GetType().Name -ne "String")
+        {            
+            [pscustomobject]$Obj = @{}
+            Foreach ($Item in $DOPerfSnap)
+            {
+                $Obj.Add($Item.Split()[0], $Item.Split()[-1])
+            }
+
+            [void]$DOPerfSnapTable.Columns.Add("Statistic")
+            [void]$DOPerfSnapTable.Columns.Add("Value")
+
+            Foreach ($Item in $Obj.Keys)
+            {
+                [void]$DOPerfSnapTable.Rows.Add($Item, $obj["$item"])
+            }
+        }
+        ElseIf ($DOPerfSnap.GetType().Name -eq "String")
+        {
+            [void]$DOPerfSnapTable.Columns.Add("Info")
+            [void]$DOPerfSnapTable.Rows.Add("$DOPerfSnap")
+        }
+        $UI.DataSource[4] = $DOPerfSnapTable
+
+
+
+        # Get the DO status
+        $UI.DataSource[1] = "Retrieving Current DO Status"
+        If ($ComputerName -eq $env:COMPUTERNAME)
+        {
+            $DOStatus = Try{Get-DeliveryOptimizationStatus -Verbose -ErrorAction Stop}Catch{}
+        }
+        Else
+        {
+            $DOStatus = Invoke-Command -Session $PSSession -ScriptBlock { Try{Get-DeliveryOptimizationStatus -Verbose -ErrorAction Stop}Catch{} }
+        }  
+        $DOStatusTable = New-Object System.Data.Datatable
+        
+        If ($DOStatus.Count -ge 1 -and $DOStatus.GetType().Name -ne "String")
+        {            
+            $Properties = $DOStatus[0] | Get-Member -MemberType Property | Select -ExpandProperty Name
+            Foreach ($Property in $Properties)
+            {
+                [void]$DOStatusTable.Columns.Add("$Property")
+            }
+            Foreach ($Item in $DOStatus)
+            {
+                $Row = $DOStatusTable.NewRow()
+                Foreach ($Property in $Properties)
+                {
+                    $Row.$Property = $Item.$Property
+                }
+                [void]$DOStatusTable.Rows.Add($Row)
+            }
+        }
+        ElseIf ($DOStatus.GetType().Name -eq "String")
+        {
+            [void]$DOStatusTable.Columns.Add("Info")
+            [void]$DOStatusTable.Rows.Add("$DOStatus")
+        }
+        $UI.DataSource[5] = $DOStatusTable
+
+        # Finish up
+        $UI.DataSource[0] = $False
+        $UI.DataSource[1] = "Completed"
+        $UI.DataSource[2] = "Black"
+        If ($PSSession)
+        {
+            Remove-PSSession $PSSession
+        }
+    }
+
+    $ComputerName = $UI.ComputerName.Text
+
+    # Start a background job
+    $Job = [BackgroundJob]::New($Code,@($UI,$ComputerName))
+    $UI.Jobs += $Job
+    $Job.Start()
+
+})
